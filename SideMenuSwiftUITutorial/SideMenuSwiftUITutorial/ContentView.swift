17b78fe54536786d545b3f2c00ed9183bc96e54c
user210249 <user210249@TX049.local>
2022-01-28 20:44:34 -0600

    init version

---
+++ SideMenuSwiftUITutorial/SideMenuSwiftUITutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SideMenuSwiftUITutorial
+//
+//  Created by user210249 on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SideMenuSwiftUITutorial/SideMenuSwiftUITutorial/SideMenuSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuSwiftUITutorialApp.swift
+//  SideMenuSwiftUITutorial
+//
+//  Created by user210249 on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
